006abe609764c0d338fba63232c518ee232064f0
VohsiLiu <alan.turing.von.neumann@gmail.com>
2022-01-14 20:51:32 +0800

    DS 217 53

---
+++ Data Structure/217 Contains Duplicate/cmake-build-debug/CMakeFiles/217_Contains_Duplicate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "217_Contains_Duplicate"
+  "217_Contains_Duplicate.pdb"
+  "CMakeFiles/217_Contains_Duplicate.dir/217_Contains_Duplicate.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/217_Contains_Duplicate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Data Structure/53 Maximum Subarray/53 Maximum Subarray.cpp
@@ -0,0 +1,64 @@
+#include <sstream>
+#include "iostream"
+#include "vector"
+#include "string"
+#include "algorithm"
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp;
+        dp.push_back(nums[0]);
+        int result = nums[0];
+        for (int i=1; i<nums.size(); i++){
+            int temp = max(nums[i],dp[i-1]+nums[i]);
+            dp.push_back(temp);
+            if (temp>result){
+                result = temp;
+            }
+        }
+        return result;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+
+        int ret = Solution().maxSubArray(nums);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
+
