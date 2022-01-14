85924928a65f9176da9d519642c9edd40903befc
VohsiLiu <alan.turing.von.neumann@gmail.com>
2022-01-14 22:05:26 +0800

    1

---
+++ Data Structure/1 Two Sum/cmake-build-debug/CMakeFiles/1_Two_Sum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1_Two_Sum"
+  "1_Two_Sum.pdb"
+  "CMakeFiles/1_Two_Sum.dir/1_Two_Sum.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_Two_Sum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
